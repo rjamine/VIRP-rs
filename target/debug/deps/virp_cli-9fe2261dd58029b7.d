14f971863e8f6709ac894a75b63bfde68f17443d
rjamine <rudjuanaf@gmail.com>
2026-03-14 08:36:33 -0400

    chore: untrack target/ directory

--- target/debug/deps/virp_cli-9fe2261dd58029b7.d
+++
@@ -1,5 +0,0 @@
-/Users/bennyboy/emacs/hobbies/coding/rust/projects/virp-rs/target/debug/deps/virp_cli-9fe2261dd58029b7.d: crates/virp-cli/src/main.rs
-
-/Users/bennyboy/emacs/hobbies/coding/rust/projects/virp-rs/target/debug/deps/libvirp_cli-9fe2261dd58029b7.rmeta: crates/virp-cli/src/main.rs
-
-crates/virp-cli/src/main.rs:
--- target/debug/deps/virp_core-37c962895733ad42.d
+++
@@ -1,5 +0,0 @@
-/Users/bennyboy/emacs/hobbies/coding/rust/projects/virp-rs/target/debug/deps/virp_core-37c962895733ad42.d: crates/virp-core/src/lib.rs
-
-/Users/bennyboy/emacs/hobbies/coding/rust/projects/virp-rs/target/debug/deps/libvirp_core-37c962895733ad42.rmeta: crates/virp-core/src/lib.rs
-
-crates/virp-core/src/lib.rs:
